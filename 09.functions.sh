a708163701ad2fced915e6a8f6e926968cfe8e41
shiva <shivathoodi@gmail.com>
2023-08-22 23:47:39 +0530

    changes in variable1

---
+++ 09.functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+ 
+ if [ $USERID -ne 0 ]
+ then 
+     echo "ERROR:: Please run this script with root access"
+ fi
+     
+yum install mysql -y
+
+if [ $? -ne 0 ] 
+then 
+      echo "installation of mysql is error"
+      exit 1
+else
+   
+      echo "installation of mysql is success"
+ fi   
+
+ yum install postfix -y
+
+if [ $? -ne 0 ] 
+then 
+      echo "installation of postfix is error"
+      exit 1
+else 
+      echo "installation of postfix is success"
+ fi  
