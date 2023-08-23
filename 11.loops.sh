3ffe2da1d1ed4cf9e9c088d161ada3e645ff2984
shiva <shivathoodi@gmail.com>
2023-08-23 12:50:17 +0530

    changes in loops

--- 11.loops.sh
+++ 11.loops.sh
@@ -19,15 +19,15 @@ then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 
- VALIDATE []{
-     if [ $1 -ne 0 ];
+VALIDATE []{
+    if [ $1 -ne 0 ];
     then
         echo -e "Installing $2 ...$R FALIURE $N"
         exit 1
     else 
         echo -e "Installing $2 ...$G SUCCESS $N" 
     fi  
-  } 
+} 
     
 
 
